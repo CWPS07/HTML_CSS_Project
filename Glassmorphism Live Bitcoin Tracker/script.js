eaa85d5d9ca9e58d9a3ac1d6c721a56aef1d3199
CWPS07 <doreps07@gmail.com>
2026-05-25 22:51:30 +0530

    Update script.js

--- Glassmorphism Live Bitcoin Tracker/script.js
+++ Glassmorphism Live Bitcoin Tracker/script.js
@@ -1,1 +1,23 @@
+const priceElement = document.getElementById('crypto-price');
 
+async function updatePrice() {
+    try {
+        const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd');
+        
+        if (!response.ok) throw new Error('Network issue or API limit');
+        
+        const data = await response.json();
+        const usdPrice = data.bitcoin.usd;
+
+        priceElement.innerText = `$${usdPrice.toLocaleString()}`;
+        
+    } catch (error) {
+        priceElement.innerText = "$——,——";
+        priceElement.style.color = "#ff2a6d";
+        console.error("Error fetching data:", error);
+    }
+}
+
+window.addEventListener('DOMContentLoaded', updatePrice);
+
+setInterval(updatePrice, 5000);
