c63f6d645fdd8caa09a15931cac6a8c605c1675d
CWPS07 <doreps07@gmail.com>
2026-06-02 16:31:59 +0530

    Update script.js

--- Typing Text Effect Website/script.js
+++ Typing Text Effect Website/script.js
@@ -1,1 +1,40 @@
+    const words = [
+      'Frontend Developer',
+      'UI/UX Designer',
+      'JavaScript Creator',
+      'Creative Web Designer',
+      'Typing Animation Expert'
+    ];
 
+    let wordIndex = 0;
+    let charIndex = 0;
+    let isDeleting = false;
+
+    const textElement = document.getElementById('text');
+
+    function typeEffect() {
+      const currentWord = words[wordIndex];
+
+      if (isDeleting) {
+        textElement.textContent = currentWord.substring(0, charIndex--);
+      } else {
+        textElement.textContent = currentWord.substring(0, charIndex++);
+      }
+
+      let speed = isDeleting ? 70 : 120;
+
+      if (!isDeleting && charIndex === currentWord.length + 1) {
+        speed = 1500;
+        isDeleting = true;
+      }
+
+      if (isDeleting && charIndex === 0) {
+        isDeleting = false;
+        wordIndex = (wordIndex + 1) % words.length;
+        speed = 500;
+      }
+
+      setTimeout(typeEffect, speed);
+    }
+
+    typeEffect();
